19aae4b803b16e8c1a22a08cbe0e84e0683e73cb
Aronvecs <aronvecsernyes@gmail.com>
2024-05-26 00:29:56 +0200

    Az ügyfél osztály a feladatnak megfelelő propertykkel létrehozva

---
+++ src/Validators/Costumer.java
@@ -0,0 +1,40 @@
+package Validators;
+
+public class Costumer {
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getWebshopID() {
+        return WebshopID;
+    }
+
+    public void setWebshopID(String webshopID) {
+        WebshopID = webshopID;
+    }
+
+    private int id;
+    private String name;
+    private String address;
+    private String WebshopID;
+}
